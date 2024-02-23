c852613501bd2970734c058b9ce3abeb44234ef6
Max Seelig <7907436+m-a-x-s-e-e-l-i-g@users.noreply.github.com>
2024-02-23 02:25:41 +0100

    Now a Svelte project, still a WIP

--- README.md
+++ README.md
@@ -5,5 +5,27 @@ Doesn't do what it says yet. Issa WIP. 🏗️👷
 [TO THE MOON 🚀](https://current-moon-phase-3d.netlify.app/)
 
 ## Credits
+
 - 3D Moon is forked from a Pen by Omar Emad: https://codepen.io/darkfo222/pen/QWdpqyQ
 - Moon texture by NASA's Scientific Visualization Studio: [CGI Moon Kit](https://svs.gsfc.nasa.gov/4720)
+
+## Developing
+
+Once you've created a project and installed dependencies with `npm install` (or `pnpm install` or `yarn`), start a development server:
+
+```bash
+npm run dev
+
+# or start the server and open the app in a new browser tab
+npm run dev -- --open
+```
+
+## Building
+
+To create a production version of your app:
+
+```bash
+npm run build
+```
+
+You can preview the production build with `npm run preview`.
---
+++ src/routes/+page.svelte
@@ -0,0 +1,119 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { Moon } from "lunarphase-js";
+    import * as THREE from "three";
+    import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+
+    let lunarPhase: string,
+        lunarPhaseEmoji: string,
+        lunarAge: number,
+        lunarAgePercent: number,
+        lunarDistance: number,
+        lunationNumber: number;
+
+    function updateMoonProperties() {
+        lunarPhase = Moon.lunarPhase();
+        lunarPhaseEmoji = Moon.lunarPhaseEmoji();
+        lunarAge = Moon.lunarAge(); // Earth days since the last new moon
+        lunarAgePercent = Moon.lunarAgePercent(); // Percentage of the moon's current age
+        lunarDistance = Moon.lunarDistance(); // Distance to the moon measured in units of Earth radii
+        lunationNumber = Moon.lunationNumber(); // Brown Lunation Number (BLN)
+    }
+
+    // Call the function initially to set the properties
+    updateMoonProperties();
+    setInterval(updateMoonProperties, 33);
+
+    onMount(() => {
+        var moonTextureImage = "./img/lroc_color_poles_1k.jpg";
+        var moonDisplacementImageLowRes = "./img/ldem_4_uint.jpg";
+        var moonDisplacementImageHighRes = "./img/ldem_hw5x3.jpg";
+
+        var scene = new THREE.Scene();
+
+        var camera = new THREE.PerspectiveCamera(
+            75,
+            window.innerWidth / window.innerHeight,
+            0.1,
+            1000,
+        );
+
+        var renderer = new THREE.WebGLRenderer({ antialias: true});
+
+        const controls = new OrbitControls( camera, renderer.domElement );
+        controls.enablePan = false;
+        controls.enableRotate = false;
+        controls.enableZoom = true;
+
+        renderer.setSize(window.innerWidth, window.innerHeight);
+
+        document.getElementById("moon")?.appendChild(renderer.domElement);
+
+        var geometry = new THREE.SphereGeometry(2, 60, 60);
+
+        var textureLoader = new THREE.TextureLoader();
+        var texture = textureLoader.load(moonTextureImage);
+        var displacementMap = textureLoader.load(moonDisplacementImageHighRes);
+
+        var material = new THREE.MeshPhongMaterial({
+            map: texture,
+            displacementMap: displacementMap,
+            displacementScale: 0.06,
+            bumpMap: displacementMap,
+            bumpScale: 8.04,
+            reflectivity: 0,
+            shininess: 0,
+        });
+
+        var moon = new THREE.Mesh(geometry, material);
+
+        const light = new THREE.DirectionalLight(0xffffff, 2.4);
+        light.position.set(1, 0, 0);
+        scene.add(light);
+
+        const hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.03);
+        hemiLight.color.setHSL(0.6, 1, 0.6);
+        hemiLight.groundColor.setHSL(0.095, 1, 0.75);
+        hemiLight.position.set(0, 0, 0);
+        scene.add(hemiLight);
+
+        scene.add(moon);
+        camera.position.z = 5;
+
+        moon.rotation.x = 3.1415 * 0.02;
+        moon.rotation.y = 3.1415 * 1.54;
+
+        function animate() {
+            requestAnimationFrame(animate);
+            moon.rotation.y += 0.0002;
+            moon.rotation.x += 0;
+
+            renderer.render(scene, camera);
+        }
+        animate();
+
+        // Keep moon in the middle of the window
+        function onResize() {
+            camera.aspect = window.innerWidth / window.innerHeight;
+            camera.updateProjectionMatrix();
+            renderer.setSize(window.innerWidth, window.innerHeight);
+        }
+
+        window.addEventListener("resize", onResize, false);
+    });
+</script>
+
+<svelte:head>
+    <title>{lunarPhaseEmoji} Current Moon Phase in 3D</title>
+</svelte:head>
+
+<h1>{lunarPhaseEmoji} Current Moon Phase in 3D</h1>
+<!-- print current moon phase -->
+<p>{lunarPhase + lunarPhaseEmoji}</p>
+<p>Lunar Age: {lunarAge} days</p>
+<p>Lunar Age Percent: {lunarAgePercent}</p>
+<p>Lunar Distance: {lunarDistance}</p>
+<p>Lunation Number: {lunationNumber}</p>
+
+<div id="moon"></div>
